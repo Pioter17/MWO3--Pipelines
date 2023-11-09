4efe20d7126e3071c1c33fb5f6e476deaf404ac9
Piotr Wiśniewski <44977082+Pioter17@users.noreply.github.com>
2023-11-09 13:12:11 +0100

    Delete src/main/java/mwo/shop/repositories/ProductRepository.java

--- src/main/java/mwo/shop/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package mwo.shop.repositories;
-
-import mwo.shop.models.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-    // Możesz dodać niestandardowe zapytania w tym miejscu, jeśli to konieczne
-}
