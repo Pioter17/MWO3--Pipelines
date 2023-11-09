df0bcf7348ae1c10b92c027d816439c91f933e1b
Piotr Wiśniewski <44977082+Pioter17@users.noreply.github.com>
2023-11-09 13:12:39 +0100

    Update Client.java

--- src/main/java/mwo/shop/models/Client.java
+++ src/main/java/mwo/shop/models/Client.java
@@ -64,12 +64,4 @@ public class Client {
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public List<Order> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
 }
