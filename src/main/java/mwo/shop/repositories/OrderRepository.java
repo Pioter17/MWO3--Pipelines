7a31b07ead6943467e71382f3ab33a9567fdefd0
Piotr Wiśniewski <44977082+Pioter17@users.noreply.github.com>
2023-11-09 13:05:01 +0100

    Delete src/main/java/mwo/shop/repositories/OrderRepository.java

--- src/main/java/mwo/shop/repositories/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package mwo.shop.repositories;
-
-import mwo.shop.models.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-    // Możesz dodać niestandardowe zapytania w tym miejscu, jeśli to konieczne
-}
