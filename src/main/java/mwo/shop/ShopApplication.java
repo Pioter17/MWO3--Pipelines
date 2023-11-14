7ecfa6083fab68311787a1ac145a17d2fae22c99
Pioter17 <piotrwisnie@wp.pl>
2023-11-14 18:17:15 +0100

    cos

--- src/main/java/mwo/shop/ShopApplication.java
+++ src/main/java/mwo/shop/ShopApplication.java
@@ -3,28 +3,30 @@ package mwo.shop;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
-import java.util.Properties;
-
 @SpringBootApplication
-public class ShopApplication implements CommandLineRunner {
+public class ShopApplication implements InfoContributor {
 	private static final Logger logger = LoggerFactory.getLogger(ShopApplication.class);
 
-	@Value("${info.app.version}")
+	@Value("${application.version}")
 	private String appVersion;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
+	@Override
+	public void contribute(Info.Builder builder) {
+		builder.withDetail("appVersion", appVersion);
 	}
 
-	@Override
-	public void run(String... args) {
-		System.out.println("Current application version: " + appVersion);
-		System.out.println("111111111111111111111111111!!!!!!!!!!!!!!!!!!!!!!!!111111111111111111111111111");
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(ShopApplication.class, args);
+
+		// Pobierz wartość wersji z adnotacji @Value
+		ShopApplication shopApplication = context.getBean(ShopApplication.class);
+		System.out.println("Current application version: " + shopApplication.appVersion);
 	}
 }
